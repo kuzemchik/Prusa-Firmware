796fa9b3ec8741755897cef63a7f40c525935ea4
Vladislav Kuzemchik <kuzemchik@me.com>
2019-11-26 22:32:04 -0500

    Change temps for 16 micro stepping variant too

--- Firmware/variants/1_75mm_MK3S_Bondtech-16-EINSy10a-SE_HT-Thermistor.h
+++ Firmware/variants/1_75mm_MK3S_Bondtech-16-EINSy10a-SE_HT-Thermistor.h
@@ -281,7 +281,7 @@
  *------------------------------------*/
 
 // Mintemps
-#define HEATER_0_MINTEMP 15
+#define HEATER_0_MINTEMP 5
 #define HEATER_1_MINTEMP 5
 #define HEATER_2_MINTEMP 5
 #define HEATER_MINTEMP_DELAY 15000                // [ms] ! if changed, check maximal allowed value @ ShortTimer
@@ -298,7 +298,7 @@
 #if defined(E3D_PT100_EXTRUDER_WITH_AMP) || defined(E3D_PT100_EXTRUDER_NO_AMP)
 #define HEATER_0_MAXTEMP 410
 #else
-#define HEATER_0_MAXTEMP 305
+#define HEATER_0_MAXTEMP 450
 #endif
 #define HEATER_1_MAXTEMP 305
 #define HEATER_2_MAXTEMP 305
